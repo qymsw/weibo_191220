9f177b6e5effefc83d158292375aab4439a1dc9c
qymsw <qymsw@163.com>
2019-12-21 17:45:23 +0800

    增加 CSRF 验证

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,6 +8,7 @@
       <div class="offset-md-2 col-md-8">
         <section class="user_info">
           @include('shared._user_info', ['user' => $user])
+          {{$user->email}}
         </section>
       </div>
     </div>
